9b7f00a93d6b56a880450f2df4bf9ebcb122d8fe
MSaks1 <merlesaks@hotmail.com>
2023-05-24 17:01:08 +0300

    loeng02

--- loeng02/client/script.js
+++ loeng02/client/script.js
@@ -6,16 +6,32 @@ fetch('http://localhost:8000')
 }) 
 .then ((data) => {
     console.log('õnnestus', data);
-    app.innerHTML = `
-      Title: ${data.title}<br>,
-      <img src="${data.imageUrl}"><br>,
-      description: ${data.description}<br>,
-      longitude: ${data.longitude}<br>,
-      lattitude: ${data.latitude}<br>,
-      participants:${data.participants}<br>
-    `
-})
+    for (let matk of data){
+        
+        console.log(matk);
+        app.innerHTML += `
+        <div class="matk">
+          <h2>${matk.title}</h2>
+          <div class="date"
+          <div>Kuupäev: ${matk.startTime}</div>
+          </div>
+          <div>${matk.description}</div>
+          <div>${matk.homepage}</div>
+          <a href="https://www.google.com/maps/@${matk.lat},${matk.long},17z" target="_blank">Koguneme siin!</a><br>
+          <div>Lat: ${matk.lat}</div>
+          <div>Long: ${matk.long}</div>
+          <div>Registreerunud osalejad: ${matk.participants}</div>
+          <img src="${matk.imageUrl}">
+          
+          
+        </div>
+      `;
+    }
+  })
+
+
     
 .catch(function() {
     console.log('ei õnnestunud :/');
 });
+ 
--- loeng02/server/index.js
+++ loeng02/server/index.js
@@ -5,43 +5,47 @@ const port = 8000;
 
 
 let matk1 = {
-    title: 'Oandu matkarada',
-    imageURL: 'https://lh3.googleusercontent.com/p/AF1QipOvFKn70-Crjf7CCLXi5vYXzJUnvyVqPaGs9M4b=s1360-w1360-h1020',
-    description: 'Lähme kõnnime looduses',
-    longitude: '59.56518693124138,',
-    lattitude: '26.100610726109867',
-    participants:['Merle', 'Tiina', 'Marina']
-}
+    title: 'Oandu Koprarada',
+    imageUrl: 'https://lh5.googleusercontent.com/p/AF1QipOw_8a1H3gbPNw3y_KSpgoXHdOUYUPtiS2G_UGC=w203-h152-k-no',
+    description: 'Alustame RMK Oandu looduskeskuse õuelt ',
+    startTime: '24.05.2023 KELL 12:00',
+    lat: 59.57184890332818,
+    long: 26.1117946355822963,
+    participants: ['Maarika, Klaarika, Kirsika'],
+    homepage: 'https://loodusegakoos.ee/kuhuminna/rahvuspargid/lahemaa-rahvuspark/1715'
+  }
+  
+  let matk2 = {
+    title: 'Nigula raba matkarada',
+    imageUrl: 'https://lh5.googleusercontent.com/p/AF1QipOtWLe8888g2lwPdgju_ih8uvjiiC738C7oODbu=w203-h152-k-no',
+    description: 'Koguneme parklas ja liigume koos matkarajale!',
+    startTime: '27.05.2023 KELL 14:00',
+    lat: 58.007202317060724,
+    long: 24.713864372879755,
+    participants: ['Merle, Merike,'],
+    homepage: ''
+  }
 
-let matk2 = {
-    title: 'mingi pealkiri',
-    imageURL: '',
-    description: '',
-    longitude: '',
-    lattitude: '',
-    participants:['Merle']
-}
-
-let matk3 = {
-    title: 'mingi pealkiri',
-    imageURL: '',
-    description: '',
-    longitude: '',
-    lattitude: '',
-    participants:['Merle']
-}
-
-let matkad = [matk1, matk2, matk3]
-
-const requestListener = function (req, res) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Content-Type', 'application/json');
-        res.writeHead(200);
-        res.end(JSON.stringify(matk1));
-    
-};
-
-const server = http.createServer(requestListener);
-server.listen(port, host, function() {
-    console.log(`Server is running on http://${host}:${port}`);
-});
+  let matk3 = {
+    title: 'Mukri Raba',
+    imageUrl: 'https://lh5.googleusercontent.com/p/AF1QipMgpHo1RKgyIbrLW-FO8Aa9LXbnAeGVOptORPD0=w203-h152-k-no"',
+    description: 'Koguneme parklas!',
+    startTime: '28.05.2023 KELL 12:00',
+    lat: 58.74853598721992,
+    long: 24.975054534262366,
+    participants: ['Mihkel koos perega, Janek koos perega, Ruth']
+  }
+  
+  let matkad = [matk1, matk2, matk3]
+  
+  const requestListener = function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json');
+    res.writeHead(200);
+    res.end(JSON.stringify(matkad));
+  };
+  
+  const server = http.createServer(requestListener);
+  server.listen(port, host, function () {
+      console.log(`Server is running on http://${host}:${port}`);
+  });
